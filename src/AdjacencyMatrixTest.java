a78abb53a2b4eee449a006036f32681e34c07bda
speargun <31995649+speargun@users.noreply.github.com>
2019-05-13 22:51:01 +0800

    Update AdjacencyMatrixTest.java

--- src/AdjacencyMatrixTest.java
+++ src/AdjacencyMatrixTest.java
@@ -3,115 +3,110 @@ import graph.core.IGraph;
 import graph.core.IIterator;
 import graph.core.IVertex;
 import graph.impl.AdjacencyMatrixGraph;
-/**
- * This is a file that contains some code to create a 
- * graph and test some of its methods.
- *
- * The graph is the graph of airports and the distances
- * between them that we have seen in the lectures.
- * 
- * This is an example of testing some of the methods
- * of the edge list graph implementation. 
- * 
- * You should add some more tests to test the other
- * methods that a graph implementation contains.
- */
+
 public class AdjacencyMatrixTest {
 	@SuppressWarnings("unused")
-	public static void main( String[] args ) throws Exception {
-	      IGraph<String,Integer> g = new AdjacencyMatrixGraph<String,Integer>();
-	      // create some vertices
-	      IVertex<String> hnl = g.insertVertex( "HNL" );
-	      IVertex<String> lax = g.insertVertex( "LAX" );
-	      IVertex<String> sfo = g.insertVertex( "SFO" );
-	      IVertex<String> ord = g.insertVertex( "ORD" );
-	      IVertex<String> dfw = g.insertVertex( "DFW" );
-	      IVertex<String> lga = g.insertVertex( "LGA" );
-	      IVertex<String> pvd = g.insertVertex( "PVD" );
-	      IVertex<String> mia = g.insertVertex( "MIA" );
+	public static void main(String[] args) {
+		
+		/*
+		 * Basic Test: Test some simple case
+		 * 
+		 * */
+			
+		// create some vertexes
+		//test: insertVertex() in AdjacencyMatrixGraph
+		IGraph<String,Integer> g=new AdjacencyMatrixGraph<String,Integer>();
+		IVertex<String> hnl = g.insertVertex( "HNL" );
+	    IVertex<String> lax = g.insertVertex( "LAX" );
+	    IVertex<String> sfo = g.insertVertex( "SFO" );
+	    IVertex<String> ord = g.insertVertex( "ORD" );
+	    IVertex<String> dfw = g.insertVertex( "DFW" );
+	    IVertex<String> lga = g.insertVertex( "LGA" );
+	    IVertex<String> pvd = g.insertVertex( "PVD" );
+	    IVertex<String> mia = g.insertVertex( "MIA" );
 
-	      // create some edges
-	      IEdge<Integer> hnllax = g.insertEdge( hnl, lax, 2555 );
-	      IEdge<Integer> laxsfo = g.insertEdge( lax, sfo, 337 );
-	      IEdge<Integer> ordsfo = g.insertEdge( ord, sfo, 1843 );
-	      IEdge<Integer> laxord = g.insertEdge( lax, ord, 1743 );
-	      IEdge<Integer> dfwlax = g.insertEdge( dfw, lax, 1233 );
-	      IEdge<Integer> ordpvd = g.insertEdge( ord, pvd, 849 );
-	      IEdge<Integer> dfwlga = g.insertEdge( dfw, lga, 1387 );
-	      IEdge<Integer> dfwmia = g.insertEdge( dfw, mia, 1120 );
-	      IEdge<Integer> lgamia = g.insertEdge( lga, mia, 1099 );
-	      IEdge<Integer> lgapvd = g.insertEdge( lga, pvd, 142 );
+	    // create some edges
+	    //test: insertEdge() in AdjacencyMatrixGraph
+	    IEdge<Integer> hnllax = g.insertEdge( hnl, lax, 2555 );
+	    IEdge<Integer> laxsfo = g.insertEdge( lax, sfo, 337 );
+	    IEdge<Integer> ordsfo = g.insertEdge( ord, sfo, 1843 );
+	    IEdge<Integer> laxord = g.insertEdge( lax, ord, 1743 );
+	    IEdge<Integer> dfwlax = g.insertEdge( dfw, lax, 1233 );
+	    IEdge<Integer> ordpvd = g.insertEdge( ord, pvd, 849 );
+	    IEdge<Integer> dfwlga = g.insertEdge( dfw, lga, 1387 );
+	    IEdge<Integer> dfwmia = g.insertEdge( dfw, mia, 1120 );
+	    IEdge<Integer> lgamia = g.insertEdge( lga, mia, 1099 );
+	    IEdge<Integer> lgapvd = g.insertEdge( lga, pvd, 142 );
+	    
+	    //test: areAdjacent() in AdjacencyMatrixGraph
+	    if ( g.areAdjacent( sfo,  ord ) ) {
+	    	System.out.println( "SFO and ORD adjacent: correct" );
+	    }else {
+	    	System.out.println( "SFO and ORD adjacent: incorrect" );
+	    }
 	      
-	      // sample test for areAdjacent
-	      if ( g.areAdjacent( sfo,  ord ) )
-	         System.out.println( "SFO and ORD adjacent: correct" );
-	      else
-	         System.out.println( "SFO and ORD adjacent: incorrect" );
+	    //test: endVertices() in AdjacencyMatrixGraph
+	    IVertex<String>[] ends = g.endVertices( laxord );
+	    if ( ( ends[0] == lax && ends[1] == ord ) ||
+	         ( ends[1] == lax && ends[0] == ord ) ) {
+	    	 System.out.println( "End vertices of LAX<->ORD: correct" );
+	    }else {
+	    	System.out.println( "End vertices of LAX<->ORD: incorrect" );
+	    }
+	    
+	    // test: opposite() in AdjacencyMatrixGraph
+	     if ( g.opposite( pvd, lgapvd ) == lga ) {
+	    	System.out.println( "Opposite of PVD along LGA<->PVD: correct" ); 
+	     }else {
+	    	System.out.println( "Opposite of PVD along LGA<->PVD: incorrect" ); 
+	     }
+	    
+	     //test: get the element of one vertex in AdjacencyMatrixGraph 
+	     String miaElement = mia.element();
+	     System.out.println( "Element of MIA is: " + miaElement );
 	      
-	      // sample test for endVertices
-	      IVertex<String>[] ends = g.endVertices( laxord );
-	      if ( ( ends[0] == lax && ends[1] == ord ) ||
-	           ( ends[1] == lax && ends[0] == ord ) )
-	         System.out.println( "End vertices of LAX<->ORD: correct" );
-	      else
-	         System.out.println( "End vertices of LAX<->ORD: incorrect" );
-	      
-	      // sample test for opposite
-	      if ( g.opposite( pvd, lgapvd ) == lga )
-	         System.out.println( "Opposite of PVD along LGA<->PVD: correct" );
-	      else
-	         System.out.println( "Opposite of PVD along LGA<->PVD: incorrect" );
-	      
-	      // example of getting an object from the graph 
-	      String miaElement = mia.element();
-	      System.out.println( "Element of MIA is: " + miaElement );
-	      
-	      // the edge labels were set as type int.
-	      int dfwlaxElement = dfwlax.element();
-	      System.out.println( "Distance from DFW to LAX is: " + dfwlaxElement );
-	      
-	      // print names of all vertices
-	      IIterator<IVertex<String>> it = g.vertices();
-	      while( it.hasNext() ) {
+	     //test: get the element of one edge in AdjacencyMatrixGraph
+	     int dfwlaxElement = dfwlax.element();
+	     System.out.println( "Distance from DFW to LAX is: " + dfwlaxElement );
+
+	     //test: edges() in AdjacencyMatrixGraph
+	     IIterator<IEdge<Integer>> it1=g.edges();
+	     while(it1.hasNext()) {
+	    	 IEdge<Integer> v = it1.next();
+	         System.out.println( v.element() ); 
+	     }
+	     
+	     //test: vertices() in AdjacencyMatrixGraph
+	     IIterator<IVertex<String>> it = g.vertices();
+	     while( it.hasNext() ) {
 	         // here I must cast also, since it.next() returns an Object
 	         IVertex<String> v = it.next();
-	         System.out.println( v.element());
+	         System.out.println( v.element() );
 	      }
 	      
-	      //test removing edges
-	      g.removeEdge(lgapvd);
-	      System.out.println( "edge lgapvd removed" );
-	      if ( g.areAdjacent( lga,  pvd ) )
-	          System.out.println( "LGA and PVD adjacent: correct" );
-	       else
-	          System.out.println( "LGA and PVD adjacent: incorrect" );
-	      g.removeVertex(hnl);
-	      System.out.println( "vertex hnl removed" );
-	      g.removeVertex(dfw);
-	      System.out.println( "vertex dfw removed" );
-	      hnl = g.insertVertex( "HNL" );
-	      // sample test for areAdjacent
-	      if ( g.areAdjacent( sfo,  ord ) )
-	         System.out.println( "SFO and ORD adjacent: correct" );
-	      else
-	         System.out.println( "SFO and ORD adjacent: incorrect" );
-	      
-	      // sample test for endVertices
-	      ends = g.endVertices( laxord );
-	      if ( ( ends[0] == lax && ends[1] == ord ) ||
-	           ( ends[1] == lax && ends[0] == ord ) )
-	         System.out.println( "End vertices of LAX<->ORD: correct" );
-	      else
-	         System.out.println( "End vertices of LAX<->ORD: incorrect" );
-	      
-	      // sample test for opposite
-	      if ( g.opposite( pvd, lgapvd ) == lga )
-	         System.out.println( "Opposite of PVD along LGA<->PVD: correct" );
-	      else
-	         System.out.println( "Opposite of PVD along LGA<->PVD: incorrect" );
-	      
-	      // example of getting an object from the graph 
-	      miaElement = mia.element();
-	      System.out.println( "Element of MIA is: " + miaElement );
-	   }
+	     //test: replace() in AdjacencyMatrixGraph--for edges
+	     g.replace(hnllax, 30);
+	     System.out.println("The distance between HNL & LAX is "+ hnllax.element());
+	     
+	     //test: replace() in AdjacencyMatrixGraph--for vertexes
+	     g.replace(pvd, "New PVD");
+	     System.out.println("The new name of station PVD is "+pvd.element());
+	     
+	     //test: removeEdge() in AdjacencyMatrixGraph
+	     g.removeEdge(ordsfo);
+	     if ( g.areAdjacent( sfo,  ord ) ) {
+	      System.out.println( "SFO and ORD not adjacent: incorrect" );
+	     }else {
+	      System.out.println( "SFO and ORD not adjacent: correct" ); 
+	     }
+		  
+	     //test: removeVertex() in AdjacencyMatrixGraph
+	     g.removeVertex(dfw);
+	     if ( g.areAdjacent( dfw,  lax ) ) {
+	    	 System.out.println( "DFW and LAX not adjacent: incorrect" ); 
+	     }else {
+	      System.out.println( "DFW and LAX not adjacent: correct" );
+	     }
+		
+	}
 }
